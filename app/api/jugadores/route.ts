7e9d416d29cdbf7b2775991bc4b2bd53a62b80c0
Mario <mayin7759@gmail.com>
2026-04-29 00:14:44 -0600

    agregar api jugadores

---
+++ app/api/jugadores/route.ts
@@ -0,0 +1,18 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const [rows] = await db.query("SELECT * FROM jugadores");
+  return NextResponse.json(rows);
+}
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  await db.query(
+    "INSERT INTO jugadores (nombre, imagen, respuestas) VALUES (?, ?, ?)",
+    [body.nombre, body.imagen, body.respuestas]
+  );
+
+  return NextResponse.json({ mensaje: "Jugador agregado" });
+}
---
+++ public/lib/db.ts
@@ -0,0 +1,9 @@
+import mysql from "mysql2/promise";
+
+export const db = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  port: Number(process.env.MYSQLPORT),
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+});
