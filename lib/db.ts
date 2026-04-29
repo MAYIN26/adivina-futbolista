8d9150231d11cf88501a291effe021398f34bd76
Mario <mayin7759@gmail.com>
2026-04-29 00:43:25 -0600

    fix db connection

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,3 @@
 import mysql from "mysql2/promise";
 
-export const db = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  port: Number(process.env.MYSQLPORT),
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+export const db = mysql.createPool(process.env.DATABASE_URL!);
